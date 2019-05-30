8ecf9e64fd8ff2746ddbef5dfd261f24e09f9d93
Ruth Baker <ruthmoog@gmail.com>
2019-05-30 14:42:33 +0100

    Include test that bike is released

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,11 @@ require "docking_station"
 
 describe DockingStation do
   it { should respond_to :release_bike}
+
+  # Add a test to your DockingStation specification that a) gets a bike, and then b) expects the bike to be working
+  it "should return a bike when 'release_bike'" do
+    station = DockingStation.new
+    bike = Bike.new
+    expect(station.release_bike).to eq(bike)
+  end
 end
